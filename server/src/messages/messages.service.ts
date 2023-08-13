f02bc4ddcd38f089be9fe95793df5c3ba662376a
Berkcan <berkcansavur@gmail.com>
2023-08-13 17:15:32 +0300

    -updated messages service

--- server/src/messages/messages.service.ts
+++ server/src/messages/messages.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Message } from './entities/message.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-
+import { Server, Socket } from 'socket.io';
 @Injectable()
 export class MessagesService {
   constructor(
     @InjectModel('Messages') private messageModel: Model<Message>){}
-
+    private readonly connectedClients: Map<string, Socket> = new Map();
   async create(chatGroupID: string, senderUser: string, text: string) {
     const newMessage = new this.messageModel({ 
       chatGroup: chatGroupID,
       senderUser: senderUser,
       text:text });
     return await newMessage.save();
+  }
+  identify(chatGroupId: string, socket:Socket) {
+    this.connectedClients.set(chatGroupId, socket);
+  }
+
+  getClientName(clientId:string){
   }
 
   findAll() {
