bd953b029556bc087803c3896fa3a10123aa0756
Berkcan <berkcansavur@gmail.com>
2023-09-05 03:47:10 +0300

    removed unnecessary endpoints from user controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -2,17 +2,11 @@ import {
     Body, 
     Controller, 
     Post, 
-    Get, 
-    Session,
-    UseGuards,
-    Request } from '@nestjs/common';
+    Session, } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
-import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 import { UtilsService } from '../utils/utils.service';
-import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
 import { UserProfileDTO } from './dtos/user-profile.dto';
-import { CurrentUserDTO } from './dtos/current-user.dto';
 @Controller('users')
 export class UsersController {
     constructor(
@@ -32,38 +26,9 @@ export class UsersController {
             throw new Error(error);
         }
     }
-    @UseGuards(LocalAuthGuard)
-    @Post('/login')
-    loginUser(@Request() req):any{
-        console.log(req.body)
-        try {
-            console.log(req.user);
-            return {
-                User: req.user,
-                message:'User logged in successfully'
-            }
-        } catch (error) {
-            throw new Error(error.message);
-        }
-    }
     @Post('/logout')
     async logoutUser(@Session() session: any){
         session.userId = null;
         session.CurrentUser = null;
-    }
-    @UseGuards(JwtAuthGuard)
-    
-    @Get('/me')
-    getUserProfile( @Request() req){
-        try {
-            const currentUser = new CurrentUserDTO();
-            currentUser.userId = req.session.userId;
-            currentUser.userEmail = req.session.CurrentUser.email;
-            currentUser.userName = req.session.CurrentUser.name
-            return currentUser;
-            
-        } catch (error) {
-            throw new Error(error);
-        }
     }
 }
