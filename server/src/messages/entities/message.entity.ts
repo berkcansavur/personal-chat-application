c4d4b3b2f8984ca860bfb796c27e78a8951b6cc1
Berkcan <berkcansavur@gmail.com>
2023-09-10 05:46:37 +0300

    updated

--- server/src/messages/entities/message.entity.ts
+++ server/src/messages/entities/message.entity.ts
@@ -1,6 +1,4 @@
-import { Prop } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
-import { Schema as mSchema, Document, Types } from 'mongoose';
 export class Message {
     chatGroup: string;
     senderUser: string;
